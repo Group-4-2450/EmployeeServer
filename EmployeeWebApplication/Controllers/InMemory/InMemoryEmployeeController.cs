24323007f3fdd9ad3b82bae5063102ef6e6d35c7
AarronRasmussen <arasmussen96@gmail.com>
2020-10-25 12:17:25 -0600

    Created InMemory 'Controller' endpoints to be used for creating unit tests. This will allow us to test without making any network calls

---
+++ EmployeeWebApplication/Controllers/InMemory/InMemoryEmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeWebApplication.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeWebApplication.Controllers.InMemory
+{
+    public class InMemoryEmployeeController : IEmployee
+    {
+        public Task CreateEmployeeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteEmployeeRecordAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetAllEmployeesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetEmployeeAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateEmployeeAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
